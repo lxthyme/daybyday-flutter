63e4474b1bd0dbc0b6cdb20e7076fc609720ca0e
lxthyme <lx314333@gmail.com>
2019-09-27 17:09:02 +0800

    ios env

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 # flutter emulators --launch Pixel_2_API_28
 cd 'd:\360MoveData\Desktop\Demo\flutter_hw'
 
- flutter emulators --launch Nexus_5X_API_29_x86
+flutter emulators --launch Nexus_5X_API_29_x86
 
- flutter run
+flutter run
+
+
+
+#### MAC
+open -a Simulator
