360a2054c1d2fb1cf418a50705fe58c3379b18bb
LX314 <lx314333@gmail.com>
2019-10-30 18:41:20 +0800

    demo in official doc

---
+++ lib/example/ShoppingList.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Product {
+  final String name;
+  const Product({this.name});
+}
+typedef void CartChangedCallback(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+  ShoppingListItem({Product product, this.inCart, this.onCartChanged})
+   : product = product,
+   super(key: ObjectKey(product));
+
+   Color _getColor(BuildContext context) {
+          return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+   }
+
+   TextStyle _getTextStyle(BuildContext context) {
+     if(!inCart) return null;
+
+     return TextStyle(
+       color: Colors.black54,
+       decoration: TextDecoration.lineThrough,
+     );
+   }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        onCartChanged(product, inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[0]),
+      ),
+      title: Text(product.name, style: _getTextStyle(context)),
+    );
+  }
+}
+
+
+class ShoppingList extends StatefulWidget {
+  ShoppingList({Key key, this.products}) : super(key: key);
+
+  final List<Product> products;
+
+  @override
+  _ShoppingListState createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  Set<Product> _shoppingCart = Set<Product>();
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if(!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shopping List'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((Product product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+            );
+        }).toList(),
+      ),
+    );
+  }
+}
