08cae1412ff63c4d30b1413dd277305cd027383c
lxthyme <1094426094@qq.com>
2019-11-04 20:28:03 +0800

    updated

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ cd 'd:\360MoveData\Desktop\Demo\flutter_hw'
 # flutter emulators --launch Nexus_5X_API_29_x86
 flutter emulators --launch Pixel_2_API_25 --enable-software-rendering
 
-flutter run
+flutter run --track-widget-creation
 
 
 
